0ae3a22d9a6a1971e8ab71cc456cca60713efef2
Damian Temimi <damian.temimi@gmail.com>
2023-11-19 14:42:34 +0100

    Poprawki do zadania

--- src/main/java/org/example/Square.java
+++ src/main/java/org/example/Square.java
@@ -1,19 +1,7 @@
 package org.example;
 
-public class Square extends Shape {
-    private double side;
-
-    public Square(double side) {
-        this.side = side;
-    }
-
-    @Override
-    public double getArea() {
-        return Math.round(side * side * 100.0) / 100.0;
-    }
-
-    @Override
-    public double getPerimeter() {
-        return Math.round(4 * side * 100.0) / 100.0;
+public class Square extends Rectangle {
+    public Square(double sideLength) {
+        super(sideLength, sideLength);
     }
 }
